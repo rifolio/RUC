63e865bc6c8d21ef219944c290df8298dab235a7
Vladyslav Horbatenko <87945469+rifolio@users.noreply.github.com>
2023-10-18 19:06:46 +0200

    Create Classwork folder
    With my first lesson code

---
+++ Courses Exercises and More/Essential Computing/Classwork/hellouniverse.java
@@ -0,0 +1,11 @@
+public class hellouniverse {
+    public static void main(String[] args) {
+        // generating some simple outputs
+        System.out.println("Hello, Universe!");
+        // using syntax to output \ and " symbols
+        System.out.println("\\\\//\n//\\\\");
+        // displaying multiple lines of output with one line of code
+        System.out.println("\\n gives you a \"new line\\n\\t becomes \"tab\"");
+    }
+
+}
